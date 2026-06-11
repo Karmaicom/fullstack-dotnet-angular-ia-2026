378585bade7d6a129be857d8335c3d7620ab1e27
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-11 15:46:42 -0300

    Criacao da classe de entidade Cliente; Implementacao da tela do prompt solicitando e armazenando os dados; Mostragem dos dados no console.

---
+++ aula01/CadastroDeClientes/CadastroDeClientes/Entidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroDeClientes.Entidades
+{
+    public class Cliente
+    {
+        
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public DateTime DataHoraCadastro { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ aula01/CadastroDeClientes/CadastroDeClientes/Program.cs
@@ -0,0 +1,38 @@
+﻿using CadastroDeClientes.Entidades;
+
+namespace CadastroDeClientes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nSeja bem vindo ao sistema de cadastro de clientes!\n");
+            Console.WriteLine("\tPreencha os campos abaixo para cadastrar o cliente:\n");
+
+            // Instaciando um objeto do tipo Clinte
+            var cliente = new Cliente();
+
+            // Atribuindo valores aos atributos do objeto cliente
+            Console.Write("Nome............: ");
+            cliente.Nome = Console.ReadLine() ?? string.Empty;
+
+            Console.Write("Email...........: ");
+            cliente.Email = Console.ReadLine() ?? string.Empty;
+
+            Console.Write("CPF.............: ");
+            cliente.Cpf = Console.ReadLine() ?? string.Empty;
+
+            Console.Write("Telefone........: ");
+            cliente.Telefone = Console.ReadLine() ?? string.Empty;
+
+            // Imprimindo os dados do cliente cadastrado
+            Console.WriteLine("\nDados do Cliente:\n");
+            Console.WriteLine("\tID...........................: " + cliente.Id);
+            Console.WriteLine("\tNome.........................: " + cliente.Nome);
+            Console.WriteLine("\tEmail........................: " + cliente.Email);
+            Console.WriteLine("\tCPF..........................: " + cliente.Cpf);
+            Console.WriteLine("\tTelefone.....................: " + cliente.Telefone);
+            Console.WriteLine("\tData e hora do cadastro......: " + cliente.DataHoraCadastro);
+        }
+    }
+}
